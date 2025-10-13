2c1078dec249f8b3dac6763d68447073f5cfe2fa
Ahmet Tarik Duyar(Atduyar) <atduyar@gmail.com>
2025-10-13 18:56:08 +0300

    feat: Split the main function. new TODOs are added.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
 #![allow(dead_code, unused)]
 use std::{collections::HashMap, io::{self, Write}};
 
-// <function> <string> <KVValue>
-// SET "ahmet" 23
-//
-// SET 
-// "ahmet" 
-// 23
-//
-// [SET STRING("ahmet") NUMBER(23)]
 #[derive(Debug)]
 enum Token {
     Commands(Commands),
@@ -31,62 +23,98 @@ enum KVValue {
     BOOL(bool)
 }
 
+// SET "ahmet" 23
+//
+// SET 
+// "ahmet" 
+// 23
+//
+// [SET STRING("ahmet") NUMBER(23)]
+fn parse_string(input: String) -> Vec<Token> {
+    // TODO: "ahmet Tarik" 2 token olarak aliyor. Bu tek Token olmali.
+    // Atduyar
+    let token_strings = input.split_whitespace().collect::<Vec<&str>>();
+    let mut tokens: Vec<Token> = Vec::new();
+
+    for token in token_strings {
+        match token {
+            "SET" => tokens.push(Token::Commands(Commands::SET)),
+            "GET" => tokens.push(Token::Commands(Commands::GET)),
+            "DEL" => tokens.push(Token::Commands(Commands::DEL)),
+            "ADD" => tokens.push(Token::Commands(Commands::ADD)),
+            "SUB" => tokens.push(Token::Commands(Commands::SUB)),
+            "true"  => tokens.push(Token::Value(KVValue::BOOL(true))),
+            "false" => tokens.push(Token::Value(KVValue::BOOL(false))),
+            _ => {
+                match token.get(..1) {
+                    Some("\"") => {
+                        let mut string_literal = token.to_string();
+                        if token.chars().last().unwrap_or(' ') != '"' {
+                            println!("Invalid String.");
+                            break;
+                        }
+                        tokens.push(Token::Value(KVValue::STRING(string_literal.trim_matches(&['"'] as &[_]).to_string())));
+                    },
+                    Some("0")|
+                    Some("1")|
+                    Some("2")|
+                    Some("3")|
+                    Some("4")|
+                    Some("5")|
+                    Some("6")|
+                    Some("7")|
+                    Some("8")|
+                    Some("9")
+                    => {
+                        // TODO: int parse need to be chacked. eg. token "2test" start with
+                        // number but is not a valid number.
+                        // Kayra
+                        let token = Token::Value(KVValue::NUMBER(token.parse::<i64>().unwrap()));
+                        tokens.push(token)
+                    },
+                    _ => println!("Unkonwn token.")
+                }
+            }
+        }
+    }
+    return tokens;
+}
+
+// SET "ahmet" 42
+// SET <string> <KVValue>
+// GET <string>
+// DEL <string>
+// ADD <string>
+// ADD <string> <Number>
+fn interpret_tokens(tokens: Vec<Token>){
+    if tokens.is_empty() {
+        return;
+    }
+    if let Some(Token::Value(_)) = tokens.get(0) {
+        println!("ERROR: First token cannot be a Value");
+        return;
+    }
+}
+
 fn main() {
     loop {
         let mut input = String::new();
         print!(">> ");
         io::stdout().flush().expect("Failed to flush stdout");
+
+        // TODO: stdin can be closed. in this case of None program should escape from the loop.
+        // Taylan
         io::stdin().read_line(&mut input).unwrap();
 
         if (input.trim() == ".quit") {
             break;
         }
 
-        // TODO: "ahmet Tarik" 2 token olarak aliyor. Bu tek Token olmali.
-        let token_strings = input.split_whitespace().collect::<Vec<&str>>();
-        let mut tokens: Vec<Token> = Vec::new();
+        let tokens = parse_string(input);
 
-        for token in token_strings {
-            match token {
-                "SET" => tokens.push(Token::Commands(Commands::SET)),
-                "GET" => tokens.push(Token::Commands(Commands::GET)),
-                "DEL" => tokens.push(Token::Commands(Commands::DEL)),
-                "ADD" => tokens.push(Token::Commands(Commands::ADD)),
-                "SUB" => tokens.push(Token::Commands(Commands::SUB)),
-                "true"  => tokens.push(Token::Value(KVValue::BOOL(true))),
-                "false" => tokens.push(Token::Value(KVValue::BOOL(false))),
-                _ => {
-                    match token.get(..1) {
-                        Some("\"") => {
-                            let mut string_literal = token.to_string();
-                            if token.chars().last().unwrap_or(' ') != '"' {
-                                println!("Invalid String.");
-                                break;
-                            }
-                            tokens.push(Token::Value(KVValue::STRING(string_literal.trim_matches(&['"'] as &[_]).to_string())));
-                        },
-                        Some("0")|
-                        Some("1")|
-                        Some("2")|
-                        Some("3")|
-                        Some("4")|
-                        Some("5")|
-                        Some("6")|
-                        Some("7")|
-                        Some("8")|
-                        Some("9")
-                        => {
-                            let token = Token::Value(KVValue::NUMBER(token.parse::<i64>().unwrap()));
-                            tokens.push(token)
-                        },
-                        _ => println!("Unkonwn token.")
-                    }
-                }
-            }
-        }
+        println!("TOKENS = {:?}", tokens);
 
-        println!("TOKENS = {:?}", tokens)
-            
+        interpret_tokens(tokens);
     }
 }
 
